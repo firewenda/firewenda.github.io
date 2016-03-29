4a1b7c28af0d8a77d5035ab659b9fd94af7f365b
wenda <btlitong@gmail.com>
2016-03-29 21:51:29 +0800

    update query
    update query

--- _site/js/post.js
+++ _site/js/post.js
@@ -47,7 +47,7 @@ $(document).ready(function(){
     });
 
     (function(){
-        var ie6 = ($.browser.msie && $.browser.version=="6.0") ? true : false;
+        //var ie6 = ($.browser.msie && $.browser.version=="6.0") ? true : false;
 
         function initHeading(){
             var h2 = [];
@@ -129,7 +129,7 @@ $(document).ready(function(){
             };
         })();
 
-        if($('.entry h2').length > 2 && !isMobile.any() && !ie6){
+        if($('.entry h2').length > 2 && !isMobile.any()){
 
             genIndex();
 
